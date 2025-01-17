02c81c31c6a29eb56b26a3b302f5c6932810d217
vihagasm17 <vihagasm17@gmail.com>
2025-01-17 15:25:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "SiIihLxC#5QfA8FQ-2Y83l00mLpeA6tETIQj0oGM-QdyDz6r-4LY", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUES_REPLY: process.env.STATUES_REPLY || "true",
@@ -16,9 +16,9 @@ FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",   
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+READ_MESSAGE: process.env.READ_MESSAGE || "true",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO,
-INBOX_BLOCK: process.env.INBOX_BLOCK || "false",
+INBOX_BLOCK: process.env.INBOX_BLOCK || "true",
 };
